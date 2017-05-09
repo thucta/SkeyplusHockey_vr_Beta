8959f10c80212f589550ac42a62abcc811d65009
nttg20 <iter.nhuttruongptit@gmail.com>
2017-05-09 13:25:42 +0700

    update deskop

--- SkyplusHockey/desktop/src/com/skyplus/hockey/desktop/DesktopLauncher.java
+++ SkyplusHockey/desktop/src/com/skyplus/hockey/desktop/DesktopLauncher.java
@@ -2,13 +2,11 @@ package com.skyplus.hockey.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.skyplus.hockey.Hockey;
 import com.skyplus.hockey.network.GameListener;
 import com.skyplus.hockey.objects.DeviceAPI;
 import com.skyplus.hockey.objects.Room;
 
-import java.net.InetAddress;
 import java.util.List;
 
 /**
@@ -33,7 +31,7 @@ public class DesktopLauncher implements DeviceAPI{
 	@Override
 	public String getIpAddress() {
 
-		return "192.168.0.103";
+		return "192.168.43.28";
 	}
 
 	@Override
@@ -51,16 +49,28 @@ public class DesktopLauncher implements DeviceAPI{
 
 	}
 
+
 	@Override
 	public void showAlertDialog(String message) {
 
 	}
 
-
 	@Override
-	public void showProgressDialog(String message) {
+	public void showAlertDialogDisconnected(GameListener gameListener, String message) {
 
 	}
+
+	@Override
+	public void showAlertDialogExitGame(GameListener gameListener, String message) {
+
+	}
+
+	@Override
+	public void showProgressDialog(GameListener gameListener, String message) {
+
+	}
+
+
 
 	@Override
 	public void closeProgressDialog() {
@@ -79,19 +89,10 @@ public class DesktopLauncher implements DeviceAPI{
         gameListener.createServer("Sas");
 	}
 
-
 	@Override
-	public void startRecording() {
+	public void vibRate(int miliSecond) {
 
 	}
 
-	@Override
-	public void transmit(byte[] message) {
 
-	}
-
-	@Override
-	public int getBufferSize() {
-		return 0;
-	}
 }
