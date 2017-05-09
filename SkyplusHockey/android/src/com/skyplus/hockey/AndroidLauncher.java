8d30498a2394c2afc47df90c6e5a8cfe36ca92b6
nttg20 <iter.nhuttruongptit@gmail.com>
2017-05-09 13:18:06 +0700

    update laucher android

--- SkyplusHockey/android/src/com/skyplus/hockey/AndroidLauncher.java
+++ SkyplusHockey/android/src/com/skyplus/hockey/AndroidLauncher.java
@@ -7,12 +7,16 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -31,8 +35,12 @@ import com.skyplus.hockey.objects.Room;
 import java.math.BigInteger;
 import java.math.RoundingMode;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.nio.ByteOrder;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
 
 /**
@@ -40,9 +48,9 @@ import java.util.List;
  */
 public class AndroidLauncher extends AndroidApplication implements DeviceAPI {
 
-    private int minBufferSize = 0;
-    private final int maxBufferSize = 4096;
+
     private ProgressDialog progressDialog = null;
+    private Vibrator vibrator = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,27 +61,47 @@ public class AndroidLauncher extends AndroidApplication implements DeviceAPI {
 
     // get ip cua thiet bi
     @Override
-    public String getIpAddress() {
-        WifiManager wifiMan = (WifiManager) getContext().getSystemService(getContext().WIFI_SERVICE);
-        int ip = wifiMan.getConnectionInfo().getIpAddress();
-
-        if (ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN)) {
-            ip = Integer.reverseBytes(ip);
-        }
-
-        byte[] ipByteArray = BigInteger.valueOf(ip).toByteArray();
-
-        String ipAddress;
+    public String getIpAddress( ) {
+//        WifiManager wifiMan = (WifiManager) getContext().getSystemService(getContext().WIFI_SERVICE);
+//        int ip = wifiMan.getConnectionInfo().getIpAddress();
+//
+//        if (ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN)) {
+//            ip = Integer.reverseBytes(ip);
+//        }
+//
+//        byte[] ipByteArray = BigInteger.valueOf(ip).toByteArray();
+//
+//        String ipAddress;
+//        try {
+//            ipAddress = InetAddress.getByAddress(ipByteArray).getHostAddress();
+//        } catch (UnknownHostException e) {
+//            ipAddress = "Unable to get host address";
+//        }
+//
+//        return ipAddress;
+        String ipAddress = null;
         try {
-            ipAddress = InetAddress.getByAddress(ipByteArray).getHostAddress();
-        } catch (UnknownHostException e) {
-            ipAddress = "Unable to get host address";
-        }
-
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        ipAddress = inetAddress.getHostAddress().toString();
+                    }
+                }
+            }
+        } catch (SocketException ex) {}
         return ipAddress;
     }
+
     @Override
-    public void closeProgressDialog(){
+    public void vibRate(int milliSecond){
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 400 milliseconds
+        vibrator.vibrate(milliSecond);
+    }
+    @Override
+    public void closeProgressDialog() {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -91,25 +119,39 @@ public class AndroidLauncher extends AndroidApplication implements DeviceAPI {
 
     @Override
     public boolean progressDialogIsShow() {
-        return progressDialog!=null;
+        return progressDialog != null;
     }
 
+
     @Override
-    public void showProgressDialog(final String message){
+    public void showProgressDialog(final GameListener gameListener, final String message) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                if(progressDialog == null){
+                if (progressDialog == null) {
                     progressDialog = new ProgressDialog(getContext());
                     progressDialog.setIndeterminate(true);
                     progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
                     progressDialog.setMessage(message);
+                    progressDialog.setIndeterminate(true);
+                    progressDialog.setTitle(Hockey.TITLE);
+                    progressDialog.setOnKeyListener(new ProgressDialog.OnKeyListener() {
+                        @Override
+                        public boolean onKey(DialogInterface dialogInterface, int i, KeyEvent keyEvent) {
+                            if (i == KeyEvent.KEYCODE_BACK) {
+                                gameListener.backProgesDialog();
+                            }
+                            return false;
+                        }
+                    });
+                    progressDialog.setCanceledOnTouchOutside(false);
                 }
                 progressDialog.show();
             }
         });
 
     }
+
     // kiem tra thiet bi co ket noi mang chua
     @Override
     public boolean isConnectedToLocalNetwork() {
@@ -129,20 +171,20 @@ public class AndroidLauncher extends AndroidApplication implements DeviceAPI {
     }
 
     @Override
-    public void showAlertDialog(final GameListener listener,final List<Room> listRoom) {
+    public void showAlertDialog(final GameListener listener, final List<Room> listRoom) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                ArrayAdapter<Room> adapter = new ArrayAdapter<Room>(getContext(),android.R.layout.select_dialog_singlechoice,listRoom);
+                ArrayAdapter<Room> adapter = new ArrayAdapter<Room>(getContext(), android.R.layout.select_dialog_singlechoice, listRoom);
                 builder.setTitle(Hockey.TITLE);
                 builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                Room room = listRoom.get(which);
-                                listener.connectServer(room);
-                                dialog.dismiss();
-                            }
-                        });
+                    public void onClick(DialogInterface dialog, int which) {
+                        Room room = listRoom.get(which);
+                        listener.connectServer(room);
+                        dialog.dismiss();
+                    }
+                });
                 builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         dialog.cancel();
@@ -166,6 +208,75 @@ public class AndroidLauncher extends AndroidApplication implements DeviceAPI {
                         dialog.cancel();
                     }
                 });
+
+                builder.create().show();
+            }
+        });
+    }
+
+    @Override
+    public void showAlertDialogDisconnected(final GameListener gameListener, final String message) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle(Hockey.TITLE);
+                builder.setMessage(message);
+                builder.setNegativeButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        gameListener.onConnectionFailed();
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.setOnKeyListener(new DialogInterface.OnKeyListener() {
+                    @Override
+                    public boolean onKey(DialogInterface dialogInterface, int i, KeyEvent keyEvent) {
+                        if (i == KeyEvent.KEYCODE_BACK) {
+                            gameListener.onConnectionFailed();
+                            dialogInterface.dismiss();
+                        }
+                        return false;
+                    }
+
+                });
+                builder.create().show();
+            }
+        });
+    }
+    @Override
+    public void showAlertDialogExitGame(final GameListener gameListener, final String message){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle(Hockey.TITLE);
+                builder.setMessage(message);
+                builder.setCancelable(false);
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        gameListener.onConnectionFailed();
+                        dialog.dismiss();
+                    }
+                });
+                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+//                builder.setOnKeyListener(new DialogInterface.OnKeyListener() {
+//                    @Override
+//                    public boolean onKey(DialogInterface dialogInterface, int i, KeyEvent keyEvent) {
+//                        if (i == KeyEvent.KEYCODE_BACK) {
+//                            gameListener.onConnectionFailed();
+//                            dialogInterface.dismiss();
+//                        }
+//                        return false;
+//                    }
+//
+//                });
                 builder.create().show();
             }
         });
@@ -184,7 +295,7 @@ public class AndroidLauncher extends AndroidApplication implements DeviceAPI {
 
                 final EditText input = (EditText) viewInflated.findViewById(R.id.input);
                 builder.setView(viewInflated);
-
+                builder.setCancelable(false);
                 builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
@@ -203,22 +314,6 @@ public class AndroidLauncher extends AndroidApplication implements DeviceAPI {
                 builder.create().show();
             }
         });
-    }
-
-
-    @Override
-    public void startRecording() {
-
-    }
-
-    @Override
-    public void transmit(byte[] message) {
-
-    }
-
-    @Override
-    public int getBufferSize() {
-        return Math.max(minBufferSize, maxBufferSize);
     }
 
 
